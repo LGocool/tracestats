43b90ae73c0edfb4d39a7a2e78e31fd592d8c1e1
LGocool <67392650+LGocool@users.noreply.github.com>
2020-08-10 10:29:05 -0400

    Add files via upload

---
+++ distributions.py
@@ -0,0 +1,26 @@
+trace=open(trace_medium,'rt')
+req= trace.readlines()
+
+distance={}
+total_distances={
+"distance 1":0
+"distance 2":0
+"distance 3":0
+"distance 4":0
+}
+count=0
+for row in rows:
+    obj_id= row[1]
+    count = 0;
+ if obj_id not in distance:
+         distances[obj_id[i]] = 0
+if obj_id in distance:
+if obj_id[i] == obj_id[i + 1]:
+    count+=1
+else:
+    obj_id[i]=0
+
+key=count
+if key in total_distances:
+    total_distances[key]+=1
+
